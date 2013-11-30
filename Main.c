3dc62243b1871fdc22453a935b8cad6ae1207435
Arkady92 <jakub1492@gmail.com>
2013-11-30 18:56:58 +0100

    wejscie

--- Main.c
+++ Main.c
@@ -26,20 +26,16 @@ void display_students();
 //Main function
 int main() 
 {
-	char command[20];
 	int counter = 0;
 	int i,j;
 	char parameters[10][100];
 	char command_sign;
-	int command_info = 1;
-	int arguments = -1;
+	int arguments = 0;
 	int running = 1;
 	int space_pos = 0;
 	for(i = 0; i<10; i++)
 		for(j = 0; j<100; j++)
 			parameters[i][j] = NULL;
-	for(i= 0; i<20; i++)
-		command[i] = NULL;
 	while(running)
 	{
 		for(;;) 
@@ -49,99 +45,91 @@ int main()
 				break;
 			if(command_sign == ' ')
 			{
-				if(command_info)
-					command_info = 0;
 				arguments++;
 				counter = 0;
 				continue;
 			}
-			if(command_info)
-			{
-				command[counter++] = command_sign;
-			}
-			else
-			{
-				if(command_sign == '(' || command_sign == ')' || command_sign == '\n')
-					continue;
-				parameters[arguments][counter++] = command_sign;
-			}
+			if(command_sign == '(' || command_sign == ')' || command_sign == '\n')
+				continue;
+			parameters[arguments][counter++] = command_sign;
 		}
 	}
+
 	system("pause");
 	return 0;
 }
 /*void add_student(int index, char* First_name, char* Last_name)
 {
-	int i = 0;
-	FILE *fp = NULL;
-	for(i=0;i<students_act_count;i++)
-	{
-		if(students[i]->index == index)
-		{
-			printf("Already exist");
-			return;
-		}
-	}
-	if ((fopen_s(&fp,data_file,"r+"))) {
-		printf ("Error in opening file: %s!\n", data_file);
-		getchar();
-		return ;
-	}
-	if(fp != NULL) {
-		students[students_act_count] = (Student*)malloc(sizeof(Student));
-		students[students_act_count]->index = (int)malloc(sizeof(int));
-		students[students_act_count]->index = index;
-		students[students_act_count]->First_name = (char*)malloc(strlen(First_name)+1);
-		strcpy(students[students_act_count]->First_name, First_name);
-		students[students_act_count]->Last_name = (char*)malloc(strlen(Last_name)+1);
-		strcpy(students[students_act_count]->Last_name, Last_name);
-		students_act_count++;
-		rewind(fp);
-		fprintf(fp,"%d",students_act_count);
-		fseek(fp,0,SEEK_END);
-		fprintf(fp,"\n%d %s %s", index, First_name, Last_name);
-		fclose(fp);
-	}
+int i = 0;
+FILE *fp = NULL;
+for(i=0;i<students_act_count;i++)
+{
+if(students[i]->index == index)
+{
+printf("Already exist");
+return;
+}
+}
+if ((fopen_s(&fp,data_file,"r+"))) {
+printf ("Error in opening file: %s!\n", data_file);
+getchar();
+return ;
+}
+if(fp != NULL) {
+students[students_act_count] = (Student*)malloc(sizeof(Student));
+students[students_act_count]->index = (int)malloc(sizeof(int));
+students[students_act_count]->index = index;
+students[students_act_count]->First_name = (char*)malloc(strlen(First_name)+1);
+strcpy(students[students_act_count]->First_name, First_name);
+students[students_act_count]->Last_name = (char*)malloc(strlen(Last_name)+1);
+strcpy(students[students_act_count]->Last_name, Last_name);
+students_act_count++;
+rewind(fp);
+fprintf(fp,"%d",students_act_count);
+fseek(fp,0,SEEK_END);
+fprintf(fp,"\n%d %s %s", index, First_name, Last_name);
+fclose(fp);
+}
 }
 void read_students_from_file(char* path) {
-	int i = 0;
-	int studentCount = 0;
-	FILE *fp = NULL;
-	if ((fopen_s(&fp,path,"r"))) {
-		printf ("Error in opening file: %s!\n", path);
-		getchar();
-		return ;
-	}
-	if(fp != NULL) {
-		fscanf(fp, "%d", &studentCount);
-		students = (Student**)malloc(students_max_count* sizeof(Student*));
-		while(students_act_count != studentCount)
-		{
-			if(students_act_count == students_max_count-1)
-			{
-				printf("Limit reached");
-				fclose(fp);
-				return;
-			}
-			students[students_act_count] = (Student*)malloc(sizeof(Student));
-			fscanf(fp, "%d %s %s", &index_buffer, First_name_buffer, Last_name_buffer);
-			students[students_act_count]->index = (int)malloc(sizeof(int));
-			students[students_act_count]->index = index_buffer;
-			students[students_act_count]->First_name = (char*)malloc(strlen(First_name_buffer)+1);
-			strcpy(students[students_act_count]->First_name, First_name_buffer);
-			students[students_act_count]->Last_name = (char*)malloc(strlen(Last_name_buffer)+1);
-			strcpy(students[students_act_count]->Last_name, Last_name_buffer);
-			students_act_count++;
-		}
-		fclose(fp);
-	} else {
-		puts("Unable to open file for reading!");
-	}
+int i = 0;
+int studentCount = 0;
+FILE *fp = NULL;
+if ((fopen_s(&fp,path,"r"))) {
+printf ("Error in opening file: %s!\n", path);
+getchar();
+return ;
+}
+if(fp != NULL) {
+fscanf(fp, "%d", &studentCount);
+students = (Student**)malloc(students_max_count* sizeof(Student*));
+while(students_act_count != studentCount)
+{
+if(students_act_count == students_max_count-1)
+{
+printf("Limit reached");
+fclose(fp);
+return;
+}
+students[students_act_count] = (Student*)malloc(sizeof(Student));
+fscanf(fp, "%d %s %s", &index_buffer, First_name_buffer, Last_name_buffer);
+students[students_act_count]->index = (int)malloc(sizeof(int));
+students[students_act_count]->index = index_buffer;
+students[students_act_count]->First_name = (char*)malloc(strlen(First_name_buffer)+1);
+strcpy(students[students_act_count]->First_name, First_name_buffer);
+students[students_act_count]->Last_name = (char*)malloc(strlen(Last_name_buffer)+1);
+strcpy(students[students_act_count]->Last_name, Last_name_buffer);
+students_act_count++;
+}
+fclose(fp);
+} else {
+puts("Unable to open file for reading!");
+}
 }
 void display_students(){
-	int i=0;
-	for(i=0; i<students_act_count; i++)
-		printf("%d %s %s\n",students[i]->index,students[i]->First_name, students[i]->Last_name);
+int i=0;
+for(i=0; i<students_act_count; i++)
+printf("%d %s %s\n",students[i]->index,students[i]->First_name, students[i]->Last_name);
 }*/
 
 // testing line for github
